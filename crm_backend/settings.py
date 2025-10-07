0a9a95608c255edbae714e4e58db62357e89f2c1
Hasib <hasibulislam2212@gmail.com>
2025-10-07 20:58:50 +0600

    some change

--- crm_backend/settings.py
+++ crm_backend/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", 
-     "https://crm-frontend-ten-alpha.vercel.app/", # React frontend
+     "https://crm-frontend-ten-alpha.vercel.app", # React frontend
 ]
 
 # REST framework JWT
